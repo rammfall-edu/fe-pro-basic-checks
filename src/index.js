3f18c0c6054adab8ebf14f691bfc023828a358dc
mashkovtsevasf <103369052+mashkovtsevasf@users.noreply.github.com>
2022-09-24 19:48:41 +0300

    work

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@
  * @param {string} string
  * @returns {string}
  */
-export const capitalizeString = 0;
+export const capitalizeString = (string) => string.split(' ').map((word) => `${word[0].toUpperCase()}${word.slice(1)}`).join(' ');
 
 /**
  * Должна быть function declaration
  * @param {string} string
  * @returns {string}
  */
-export const fenceString = 0;
+export const fenceString = (string) => {
+    return string.split('').map((symbol, index) => {
+    return index % 2 === 1 ? symbol.toUpperCase() : symbol.toLowerCase();
+    }).join('');
+};
 
 /**
  * Должна быть function expression
@@ -18,7 +22,18 @@ export const fenceString = 0;
  * @param {string} string
  * @returns {string}
  */
-export const reducerIf = 0;
+export const reducerIf = (action, string) =>{
+    if (action === 'uppercase'){
+        return string.toUpperCase();
+    } else if(action === 'lowercase'){
+         return string.toLowerCase();
+    } else if (action === 'capitalize'){
+         return capitalizeString (string)/*string.charAt(0).toUpperCase() + string.slice(1) альтернативный вариант*/
+    } else if (action === 'fence'){
+         return fenceString (string);
+    }
+    return string;
+  };
 
 /**
  * Стрелочная
@@ -38,7 +53,11 @@ export const consoleLoggerWordsForOf = 0;
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsFor = 0;
+ export const consoleLoggerWordsFor = (string) => {
+    for (let i=0; i < string.length; i++){
+        return string[i]
+    };
+};
 
 /**
  Стрелочная
